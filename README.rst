ecc846b157aae32f31ae4d48d5b21042c6442e15
shawnngtq <shawn.coding.acc@gmail.com>
2023-10-14 10:07:05 +0800

    Setup versioneer

---
+++ README.rst
@@ -0,0 +1,17 @@
+==============
+django-utility
+==============
+
+A Django application that provides generic utility.
+
+.. contents::
+    :local:
+    :backlinks: none
+
+Installation
+============
+``pip install django-utility``
+
+Reference
+=========
+https://docs.djangoproject.com/en/4.2/intro/reusable-apps/
---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import find_packages, setup
+
+import versioneer
+
+with open("README.rst", "r") as f:
+    LONG_DESCRIPTION = f.read()
+with open("requirements.txt", "r") as f:
+    REQUIREMENTS = [line.strip() for line in f]
+
+CLASSIFIERS = [
+    "Environment :: Web Environment",
+    "Framework :: Django",
+    "Framework :: Django :: 4.2",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.11",
+    "Topic :: Internet :: WWW/HTTP",
+    "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+]
+
+setup(
+    author_email="shawn.coding.acc@gmail.com",
+    author="Shawn Ng",
+    classifiers=CLASSIFIERS,
+    cmdclass=versioneer.get_cmdclass(),
+    description="A Django application that provides generic utility.",
+    install_requires=REQUIREMENTS,
+    license="BSD-3",
+    long_description=LONG_DESCRIPTION,
+    name="django-util",
+    packages=find_packages(include=["django_util", "django_util.*"]),
+    platforms="any",
+    test_suite="nose.collector",
+    tests_require=["nose"],
+    url="https://github.com/shawnngtq/django-util",
+    version=versioneer.get_version(),
+)
