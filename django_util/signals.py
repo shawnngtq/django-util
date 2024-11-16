13bc05febb99a4d46ed849cf8e41b538eb82cb60
shawnngtq <shawn.coding.acc@gmail.com>
2024-11-16 14:36:50 +0800

    custom Profile model

--- django_util/signals.py
+++ django_util/signals.py
@@ -1,15 +1,20 @@
+from django.apps import apps
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
-from django_util.models import Profile
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     """
-    When User object is created, create Profile object
+    When User object is created, create Profile object using the custom Profile model
+    specified in settings.PROFILE_MODEL or fallback to default Profile model
     """
+    # Get the Profile model from settings or use default
+    profile_model_path = getattr(settings, "PROFILE_MODEL", "django_util.Profile")
+    Profile = apps.get_model(profile_model_path)
+
     if created:
         Profile.objects.create(user=instance)
     else:
